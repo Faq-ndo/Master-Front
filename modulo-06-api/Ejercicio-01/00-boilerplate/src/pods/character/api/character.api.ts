a207ab38912d9b7e608f921073478a87c904e248
Facu <facundo@uptodown.com>
2023-11-11 13:11:17 +0100

    hotel replaced for characters

---
+++ modulo-06-api/Ejercicio-01/00-boilerplate/src/pods/character/api/character.api.ts
@@ -0,0 +1,10 @@
+import { Character } from './character.api-model';
+import { mockCharacterCollection } from './character.mock-data';
+
+export const getCharacter = async (id: string): Promise<Character> => {
+    return mockCharacterCollection.find((h) => h.id.toString() == id);
+};
+
+export const saveCharacter = async (character: Character): Promise<boolean> => {
+    return true;
+};
---
+++ modulo-06-api/Ejercicio-01/00-boilerplate/src/pods/character/api/character.mock-data.ts
@@ -0,0 +1,44 @@
+import { Character } from './character.api-model';
+
+export const mockCharacterCollection: Character[] = [
+    {
+      id: 1,
+      name: "Rick Sanchez",
+      status: "Alive",
+      species: "Human",
+      type: "Scientist",
+      gender: "Male",
+      origin: {
+        name: "Earth (C-137)",
+        link: "https://example.com/earth-c137",
+      },
+      location: {
+        name: "Citadel of Ricks",
+        link: "https://example.com/citadel-of-ricks",
+      },
+      image: "https://example.com/rick-sanchez-image.jpg",
+      episode: ["https://example.com/episode1", "https://example.com/episode2"],
+      url: "https://example.com/rick-sanchez",
+      created: "2022-01-01T10:00:00Z",
+    },
+    {
+      id: 2,
+      name: "Morty Smith",
+      status: "Alive",
+      species: "Human",
+      type: "Adventurer",
+      gender: "Male",
+      origin: {
+        name: "Earth (C-137)",
+        link: "https://example.com/earth-c137",
+      },
+      location: {
+        name: "Earth (C-137)",
+        link: "https://example.com/earth-c137",
+      },
+      image: "https://example.com/morty-smith-image.jpg",
+      episode: ["https://example.com/episode1", "https://example.com/episode3"],
+      url: "https://example.com/morty-smith",
+      created: "2022-01-02T09:30:00Z",
+    },
+  ];
